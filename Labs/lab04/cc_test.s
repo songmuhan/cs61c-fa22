84f99b6a84b8f5684375b66a0a65a2315224654a
lenovo-ubuntu <809617759@qq.com>
2022-10-06 00:08:27 +0800

    finish lab04

--- Labs/lab04/cc_test.s
+++ Labs/lab04/cc_test.s
@@ -50,6 +50,11 @@ next_test:
 pow:
     # BEGIN PROLOGUE
     # FIXME Need to save the calle saved register(s)
+
+    # use s0 in following code. 
+    addi sp, sp, -4
+    sw s0,0(sp)
+    
     # END PROLOGUE
     li s0, 1
 pow_loop:
@@ -61,6 +66,11 @@ pow_end:
     mv a0, s0
     # BEGIN EPILOGUE
     # FIXME Need to restore the calle saved register(s)
+    
+    # restore s0
+    lw s0, 0(sp)
+    addi sp,sp,4
+
     # END EPILOGUE
     ret
 
@@ -73,8 +83,17 @@ pow_end:
 inc_arr:
     # BEGIN PROLOGUE
     # FIXME What other registers need to be saved?
-    addi sp, sp, -4
+
+
+   # addi sp, sp, -4
+   # sw ra, 0(sp)
+   # my code here
+    addi sp, sp, -12
     sw ra, 0(sp)
+    sw s0, 4(sp)
+    sw s1, 8(sp)
+
+
     # END PROLOGUE
     mv s0, a0 # Copy start of array to saved register
     mv s1, a1 # Copy length of array to saved register
@@ -87,17 +106,29 @@ inc_arr_loop:
     #
     # FIXME Add code to preserve the value in t0 before we call helper_fn
     # Also ask yourself this: why don't we need to preserve t1?
+
+    # my code here
+    addi sp, sp, -4
+    sw t0,0(sp)
+
     #
     jal helper_fn
     # FIXME Restore t0
+    lw t0, 0(sp)
+    addi sp, sp, 4
     # Finished call for helper_fn
     addi t0, t0, 1 # Increment counter
     j inc_arr_loop
 inc_arr_end:
     # BEGIN EPILOGUE
     # FIXME What other registers need to be restored?
+
+    # my code here   
+    lw s1,8(sp)
+    lw s0,4(sp)
     lw ra, 0(sp)
-    addi sp, sp, 4
+ #   addi sp, sp, 4
+    addi sp,sp,12
     # END EPILOGUE
     ret
 
@@ -112,12 +143,17 @@ inc_arr_end:
 helper_fn:
     # BEGIN PROLOGUE
     # FIXME: YOUR CODE HERE
+    addi sp, sp, -4
+    sw s0, 0(sp)
+  #  la a0,array
     # END PROLOGUE
     lw t1, 0(a0)
     addi s0, t1, 1
     sw s0, 0(a0)
     # BEGIN EPILOGUE
     # FIXME: YOUR CODE HERE
+    lw s0, 0(sp)
+    addi sp, sp, 4
     # END EPILOGUE
     ret
 
